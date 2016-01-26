3c8946be07a1590c7f7e2b735afa61e846bc0b7f
microelly2 <thomas@freecadbuch.de>
2016-01-26 23:11:55 +0100

    photographer case added

---
+++ miki_anim_test04.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------
+#-- miki - my kivy like creation tools
+#--
+#-- microelly 2016
+#--
+#-- GNU Lesser General Public License (LGPL)
+#-------------------------------------------------
+
+import FreeCAD, FreeCADGui
+import Animation
+import geodat.miki as miki
+reload(miki)
+
+
+s='''
+#: import Animation
+#: import Part
+#: App.newDocument("Unbenannt")
+
+Part.Box: &box
+
+Animation.Manager:
+	Label: 'Mr. Pytt'
+	start: 5
+	intervall: 50
+	Animation.Mover:
+		duration:50
+
+#: App.activeDocument().ActiveObject.obj2 = App.activeDocument().getObject('test')
+
+	Animation.Photographer:
+		duration:30
+
+#: App.activeDocument().recompute()
+#: FreeCADGui.SendMsgToActiveView("ViewFit")
+#: App.ActiveDocument.getObject('My_Manager').Proxy.run()
+
+'''
+
+m=miki.Miki()
+m.run(s)
